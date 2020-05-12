fcfc26ce06b2419489ead49d14003220b5985cb7
Christian Dimas <christian.dimas.94@gmail.com>
2020-05-12 13:38:41 +0700

    fix calling boot without params causing error

--- src/app/ng-intercom/intercom/intercom.ts
+++ src/app/ng-intercom/intercom/intercom.ts
@@ -59,7 +59,7 @@ export class Intercom {
     if (!isPlatformBrowser(this.platformId)) {
       return
     }
-    const app_id = intercomData.app_id ? intercomData.app_id : this.config.appId
+    const app_id = intercomData && intercomData.app_id ? intercomData.app_id : this.config.appId
     // Run load and attach to window
     this.loadIntercom(this.config, (event?: Event) => {
       // then boot the intercom js
